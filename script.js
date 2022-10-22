4e6b3179dffe1809de08269bb2663821cbf6127a
August <Austinmicheals26@gmail.com>
2022-10-22 03:02:57 +0100

    Modified the styles and script a bit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let playerScore = 0;
-let computerScore = 0;
-let comment = '';
-let winner = '';
+  
+    let playerChoice = '';
+    let playerScore = 0;
+    let computerScore = 0;
+    //let comment = '';
+    let winner = document.querySelector('.winner');
+    let choice = document.querySelector('.choice');
+    let display = document.querySelector('.display');
+    let score = document.querySelector('.score');
+    //let scores = ``;
+    
 
 function getComputerChoice(){
     let result = '';
@@ -18,73 +25,82 @@ function getComputerChoice(){
     return result.toLowerCase();
 }
 
-function getPlayerChoice(){
-    result = prompt('Make your choice', ).toLowerCase();
-    return result;
-}
-
 
 function playRound(player, computer){
 
-    if(computer == 'paper' && player == 'rock'){
-        computerScore += 1;
-        comment = "paper beats rock";
-    }
-    else if(computer == "paper" && player == "scissors"){
-        playerScore += 1;
-        comment = "scissors beats paper";
-    }
-    else if(computer == "paper" && player == 'paper'){
-        comment = "Go again?";
-    }
-    else if(computer == "rock" && player == 'paper'){
-        playerScore += 1;
-        comment = "paper beats rock";
-    }
-    else if(computer == "rock" && player == 'scissors'){
-        computerScore += 1;
-        comment = "rock beats scissors";
-    }
-    else if(computer == "rock" && player == 'rock'){
-        comment = "Go again?";
-    }
-    else if(computer == "scissors" && player == 'rock'){
-        playerScore += 1;
-        comment = "rock beats scissors";
-    }
-    else if(computer == "scissors" && player == 'paper'){
-        computerScore += 1;
-        comment = "scissors beats paper";
-    }
-    else if(computer == "scissors" && player == 'scissors'){
-        comment = "Go again?"
-    }
-    else{
-        comment = 'Not a valid input';
-    }
+if(computer == 'paper' && player == 'rock'){
+    computerScore += 1;
+    display.textContent = "paper beats rock";
+}
+else if(computer == "paper" && player == "scissors"){
+    playerScore += 1;
+    display.textContent = "scissors beats paper";
+}
+else if(computer == "paper" && player == 'paper'){
+    display.textContent = "Go again?";
+}
+else if(computer == "rock" && player == 'paper'){
+    playerScore += 1;
+    display.textContent = "paper beats rock";
+}
+else if(computer == "rock" && player == 'scissors'){
+    computerScore += 1;
+    display.textContent = "rock beats scissors";
+}
+else if(computer == "rock" && player == 'rock'){
+    display.textContent = "Go again?";
+}
+else if(computer == "scissors" && player == 'rock'){
+    playerScore += 1;
+    display.textContent = "rock beats scissors";
+}
+else if(computer == "scissors" && player == 'paper'){
+    computerScore += 1;
+    display.textContent = "scissors beats paper";
+}
+else{
+    display.textContent = "Go again?"
+}
 
-    console.log(`Computer: ${computer} - Player: ${player}`);
-    console.log({computerScore, playerScore, comment});
+score.textContent = `computer: ${computerScore} , player: ${playerScore}.`;
+//score.textContent = scores;
+
+choice.textContent = `Computer chose ${computer}`;
+//console.log(`Computer: ${computer} - Player: ${player}`);
+//console.log(scores, comment);
 }
 
 function game(){
-    let rounds = Number(prompt('How many rounds?', '5?'));
+    let rounds = 2;
 
     for(let i = 0; i < rounds; i++){
-        playRound(getPlayerChoice(), getComputerChoice())
+        playRound(playerChoice, getComputerChoice());
     }
 
     if(computerScore > playerScore){
-        winner = `Computer wins with a total of ${computerScore} point(s)!!`;
+        winner.textContent = `Computer wins with a total of ${computerScore} point(s)!!`;
     }
     else if(computerScore == playerScore){
-        winner = 'Nobody won.. Play again?'
+        winner.textContent = 'Nobody won.. Play again?'
     }
     else{
-        winner = `You won with a total of ${playerScore} point(s)!!`
+        winner.textContent = `You won with a total of ${playerScore} point(s)!!`
     }
 
     console.log(winner);
 }
 
-game();
+
+let buttons = Array.from(document.querySelectorAll('.btn'));
+buttons.forEach(function(button){
+
+    button.addEventListener('click', function(e){
+        playerChoice = (button.textContent).toLowerCase();
+        //console.log(playerChoice)
+
+        game()
+
+    })
+
+    
+})
